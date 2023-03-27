8f51aede1a0fd376cf543443200827becaacc797
Przemysław Ren <przemyslaw.ren@gmail.com>
2023-03-27 21:34:16 +0200

    changing structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { WeatherModule } from "./widgets/weather/weather.module";
-import { ExchangeModule } from "./widgets/exchange/exchange.module";
-import { CryptoModule } from "./widgets/crypto/crypto.module";
-import { NorrisModule } from "./widgets/norris/norris.module";
-import { DogsModule } from "./widgets/dogs/dogs.module";
-import { GotModule } from "./widgets/got/got.module";
 import { BusesModule } from "./widgets/buses/buses.module";
 
 @NgModule({
@@ -18,12 +12,6 @@ import { BusesModule } from "./widgets/buses/buses.module";
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    WeatherModule,
-    ExchangeModule,
-    CryptoModule,
-    NorrisModule,
-    DogsModule,
-    GotModule,
     BusesModule,
   ],
   providers: [],
